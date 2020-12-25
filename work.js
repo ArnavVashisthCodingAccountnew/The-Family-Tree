4c8c02a54966993e65c215ae0fa19eb3d135d7af
Arnav Vashisth <arnavvst2020@gmail.com>
2020-12-25 22:26:12 +0530

    Add files via upload

---
+++ work.js
@@ -0,0 +1,8 @@
+function tree(){
+    var member_names = [Shashi Pathak , Rohit Vashisth , Kanchan Bhardwaj Vashisth , Akshita Bhardwaj , Sumati Datta , Suvid Datta , Samir Datta , Baldev Raj Bhardwaj , Suneet Pathak , Akshaj Vashisth , Ryan Gupta , Chetan Bhardwaj ];
+    var images = []
+    var i = 0;
+document.getElementById("name_member") .innerHTML = member_names [i];
+  document.getElementById("album").src = images[i];
+   i++; 
+}
